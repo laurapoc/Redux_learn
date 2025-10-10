c101b870228351e74b0cbe1ac2e2833fcba9f7ee
Laura <laura.pociuniene@oxylabs.io>
2025-10-10 14:38:05 +0300

    Update App and use latest Activity component from react library

--- react-redux-with-vite/src/App.tsx
+++ react-redux-with-vite/src/App.tsx
@@ -1,27 +1,25 @@
-import { useState } from "react";
+import { Activity, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Activity mode={isLoading ? "visible" : "hidden"}>
+        <div>Loading...</div>
+        <button onClick={() => setIsLoading(!isLoading)}>
+          Set to not loading
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Activity>
+      <Activity mode={!isLoading ? "visible" : "hidden"}>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+
+        <button onClick={() => setIsLoading(!isLoading)}>Set to loading</button>
+      </Activity>
     </>
   );
 }
