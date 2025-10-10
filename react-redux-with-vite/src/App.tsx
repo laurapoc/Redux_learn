e1901ed896b41011d8b6c7e6305ab2d809e494d3
Laura <laura.pociuniene@oxylabs.io>
2025-10-10 14:39:51 +0300

    move toogle loading logic into function

--- react-redux-with-vite/src/App.tsx
+++ react-redux-with-vite/src/App.tsx
@@ -5,20 +5,22 @@ import "./App.css";
 function App() {
   const [isLoading, setIsLoading] = useState(false);
 
+  const toggleLoading = () => {
+    setIsLoading(!isLoading);
+  };
+
   return (
     <>
       <Activity mode={isLoading ? "visible" : "hidden"}>
         <div>Loading...</div>
-        <button onClick={() => setIsLoading(!isLoading)}>
-          Set to not loading
-        </button>
+        <button onClick={toggleLoading}>Set to not loading</button>
       </Activity>
       <Activity mode={!isLoading ? "visible" : "hidden"}>
         <div>
           <h1>Hello World</h1>
         </div>
 
-        <button onClick={() => setIsLoading(!isLoading)}>Set to loading</button>
+        <button onClick={toggleLoading}>Set to loading</button>
       </Activity>
     </>
   );
