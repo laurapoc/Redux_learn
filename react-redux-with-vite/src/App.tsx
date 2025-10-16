1192c446ba0020c2680418fd2c9f9da11f547061
Laura <laura.pociuniene@oxylabs.io>
2025-10-16 19:56:27 +0300

    react_activity_usage - add image list with second counter

--- react-redux-with-vite/src/App.tsx
+++ react-redux-with-vite/src/App.tsx
@@ -2,11 +2,14 @@ import { Activity, useState } from "react";
 
 import "./App.css";
 import { Counter } from "./components/Counter";
+import { ImageList } from "./components/ImageList";
 
 function App() {
   const [showActivityCount, setShowActivityCount] = useState(true);
   const [showCount, setShowCount] = useState(true);
   const [replaceActivity, setReplaceActivity] = useState(true);
+  const [showImageList, setShowImageList] = useState(true);
+  const [showActivityImageList, setShowActivityImageList] = useState(false);
 
   const toggleShowCount = () => {
     setShowCount(!showCount);
@@ -20,36 +23,72 @@ function App() {
     setReplaceActivity(!replaceActivity);
   };
 
+  const toggleShowImageList = () => {
+    setShowImageList(!showImageList);
+    setShowActivityImageList(false);
+  };
+
+  const toggleShowActivityImageList = () => {
+    setShowActivityImageList(!showActivityImageList);
+    setShowImageList(false);
+  };
+
   return (
-    <div className="container">
-      <div className="counterBox">
-        {showCount && <Counter />}
-        <button className="toggleButton" onClick={toggleShowCount}>
-          Toggle not Activity count
-        </button>
-      </div>
+    <>
+      <div className="container">
+        <div className="flexContainer">
+          <div className="counterBox">
+            {showCount && <Counter />}
+            <button className="toggleButton" onClick={toggleShowCount}>
+              Toggle not Activity count
+            </button>
+          </div>
 
-      <div className="counterBox">
-        <Activity mode={showActivityCount ? "visible" : "hidden"}>
-          <Counter />
-        </Activity>
-        <button className="toggleButton" onClick={toggleShowActivityCount}>
-          Toggle Activity count
-        </button>
-      </div>
+          <div className="counterBox">
+            <Activity mode={showActivityCount ? "visible" : "hidden"}>
+              <Counter />
+            </Activity>
+            <button className="toggleButton" onClick={toggleShowActivityCount}>
+              Toggle Activity count
+            </button>
+          </div>
 
-      <div className="counterBox">
-        <Activity mode={replaceActivity ? "visible" : "hidden"}>
-          <Counter />
-        </Activity>
-        <Activity mode={!replaceActivity ? "visible" : "hidden"}>
-          <div className="activityDiv">Change activity content</div>
-        </Activity>
-        <button className="toggleButton" onClick={toggleChangeActivity}>
-          Toggle replace Activity count
-        </button>
+          <div className="counterBox">
+            <Activity mode={replaceActivity ? "visible" : "hidden"}>
+              <Counter />
+            </Activity>
+            <Activity mode={!replaceActivity ? "visible" : "hidden"}>
+              <div className="activityDiv">Change activity content</div>
+            </Activity>
+            <button className="toggleButton" onClick={toggleChangeActivity}>
+              Toggle replace Activity count
+            </button>
+          </div>
+
+          <div className="counterBox">
+            <div className="actionButtons">
+              <button
+                className="toggleButton2 toggleButton"
+                onClick={toggleShowActivityImageList}
+              >
+                Toggle Activity Image List
+              </button>
+              <button className="toggleButton" onClick={toggleShowImageList}>
+                Toggle Image List
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {showImageList && <ImageList />}
+
+        {
+          <Activity mode={showActivityImageList ? "visible" : "hidden"}>
+            <ImageList />
+          </Activity>
+        }
       </div>
-    </div>
+    </>
   );
 }
 
